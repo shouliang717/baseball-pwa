fb74aed44194288dd64d0e4dc7a6520efa5f3036
shouliang717 <shouliang@hlc.edu.tw>
2025-04-23 08:22:36 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+
+const cacheName = "baseball-cache-v1";
+const filesToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
